aa6caac4c4848cb4b170adfca703c434743ecfb9
athenavi <625237127@qq.com>
2026-06-24 22:11:33 +0800

    fix: SharePage UI with Refresh + right-click menu + double-tap copy - Add Refresh toolbar button - Right-click: Copy Link, Delete Share - Double-click: copy link - ShareItem columns: icon, filename, permission, date, downloads

--- Views/SharePage.xaml.cs
+++ Views/SharePage.xaml.cs
@@ -3,6 +3,8 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using StoraDesktop.Models;
 using StoraDesktop.ViewModels;
+using System;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace StoraDesktop.Views;
 
@@ -17,19 +19,36 @@ public sealed partial class SharePage : Page
         Loaded += async (s, e) => await VM.LoadAsync();
     }
 
-    private async void OnRightTap(object s, RightTappedRoutedEventArgs e)
+    private async void OnRefresh(object s, RoutedEventArgs e) => await VM.LoadAsync();
+
+    private void OnCopyLink(object s, DoubleTappedRoutedEventArgs e)
+    {
+        var share = (e.OriginalSource as FrameworkElement)?.DataContext as ShareItem;
+        if (share == null) return;
+        CopyToClipboard(share);
+    }
+
+    private void OnRightTap(object s, RightTappedRoutedEventArgs e)
     {
         var share = (e.OriginalSource as FrameworkElement)?.DataContext as ShareItem;
         if (share == null) return;
 
         var menu = new MenuFlyout();
-        var copyItem = new MenuFlyoutItem { Text = "📋 复制链接", Tag = "copy" };
-        copyItem.Click += async (s2, e2) => { VM.CopyLinkCommand.Execute(share); };
-        var delItem = new MenuFlyoutItem { Text = "🗑 删除分享", Tag = "delete" };
-        delItem.Click += async (s2, e2) => { await VM.DeleteShareCommand.ExecuteAsync(share); };
-
+        var copyItem = new MenuFlyoutItem { Text = "Copy Link" };
+        copyItem.Click += (s2, e2) => CopyToClipboard(share);
         menu.Items.Add(copyItem);
+
+        var delItem = new MenuFlyoutItem { Text = "Delete Share" };
+        delItem.Click += async (s2, e2) => { await VM.DeleteShareCommand.ExecuteAsync(share); };
         menu.Items.Add(delItem);
+
         menu.ShowAt(ShareList, e.GetPosition(ShareList));
+    }
+
+    private static void CopyToClipboard(ShareItem share)
+    {
+        var pkg = new DataPackage { RequestedOperation = DataPackageOperation.Copy };
+        pkg.SetText(share.ShareUrl);
+        Clipboard.SetContent(pkg);
     }
 }
